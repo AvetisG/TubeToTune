56eb3d8e4dea008f61dca500208ca5ebf4ebdc80
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-05-14 21:55:25 -0400

    Revert "Commenting some of the code out (I know it is a bit hacky but the logging system is not working so this should suffice at least for now)."
    This reverts commit 0d7e3cfce1a3c58abfc7231ce8a4094612c54587.

--- TubeToTune/Scripts/app/TubeToTuneApp.js
+++ TubeToTune/Scripts/app/TubeToTuneApp.js
@@ -1,25 +1,25 @@
-﻿var TubeToTuneApp = angular.module('TubeToTuneApp', ['angular-loading-bar']);
+﻿var TubeToTuneApp = angular.module('TubeToTuneApp', ['angular-loading-bar', 'ngRoute']);
 
-//TubeToTuneApp.config(function ($routeProvider) {
+TubeToTuneApp.config(function ($routeProvider) {
 
-//    $routeProvider
-//        .when('/',
-//        {
-//            templateUrl: '/Navigation/SingleTune'
-//        })
-//        .when('/singletune',
-//        {
-//            templateUrl: '/Navigation/SingleTune'
-//        })
-//        .when('/multipletunes',
-//        {
-//            templateUrl: '/Navigation/MultipleTunes'
-//        })
-//        .otherwise(
-//        {
-//            templateUrl: '/Navigation/ErrorPage'
-//        });
-//});
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: '/Navigation/SingleTune'
+        })
+        .when('/singletune',
+        {
+            templateUrl: '/Navigation/SingleTune'
+        })
+        .when('/multipletunes',
+        {
+            templateUrl: '/Navigation/MultipleTunes'
+        })
+        .otherwise(
+        {
+            templateUrl: '/Navigation/ErrorPage'
+        });
+});
 
 TubeToTuneApp.controller('TuneConvertController', function ($scope, $http) {
 
