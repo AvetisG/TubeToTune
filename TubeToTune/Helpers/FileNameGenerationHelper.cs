876d9063612e04d38b4367f47f388aae45ebf055
kirbyfan64sos <rymg19@gmail.com>
2015-09-21 20:33:03 -0500

    Fix part of #8 (not properly escaping the pound character)

--- TubeToTune/Helpers/FileNameGenerationHelper.cs
+++ TubeToTune/Helpers/FileNameGenerationHelper.cs
@@ -9,7 +9,7 @@ namespace TubeToTune.Helpers
 	{
 		public static string RemoveIllegalPathCharacters(string path)
 		{
-			string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+			string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars()) + "#";
 			var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
 			return r.Replace(path, "").Replace("&", "and");
 		}
